009015ec02300eecb33ed93b34e3c44f3b7eb398
iamagf <agf0710@gmail.com>
2022-07-14 00:39:59 -0500

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test',
+      title: 'test2',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'test Home Page'),
+      home: const MyHomePage(title: 'test2 Home Page'),
     );
   }
 }
